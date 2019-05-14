41c6d03d8b304876c394248ebc91b3984882646b
vicse <vicse.ore@tecsup.edu.pe>
2019-05-14 16:14:05 -0500

    Initial Commit

---
+++ PokemonGo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PokemonGo
+//
+//  Created by vicse on 14/05/19.
+//  Copyright © 2019 tecsup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
